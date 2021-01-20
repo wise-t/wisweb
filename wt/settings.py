01dfc5b5bea236a7abfcc46d0f958ad4ff8c65fc
wise-t <lovjes4@gmail.com>
2021-01-20 07:56:54 +0000

    remove category from form

--- wt/settings.py
+++ wt/settings.py
@@ -129,8 +129,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
      os.path.join(BASE_DIR,'static'),
