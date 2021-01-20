12028bd7a8adfeeecd010cc996aae37e5fe5ba72
wise-t <lovjes4@gmail.com>
2021-01-20 15:25:05 +0000

    fix blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
  
-from.models import Post,Category,Comment
+from.models import Post,Comment#,Category
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Comment)
-admin.site.register(Category)
+#admin.site.register(Category)
--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-from .models import  Post,Comment,Category
+from .models import  Post,Comment#,Category
 choices=[('sport','sport'),('coding','coding'),('entertaiment','entertaiment')]
-choices=Category.objects.all().values_list('id','name')
+#choices=Category.objects.all().values_list('id','name')
 
 choices_list=[]
 for item in choices:
@@ -13,14 +13,14 @@ for item in choices:
 class PostForm(forms.ModelForm):
     class Meta:
         model=Post
-        fields=('title','title_tag','author','body')#,'category')
+        fields=('title','title_tag','author','body','category')
         widgets={
             'title':forms.TextInput(attrs={'class':'form-control'}),
             'title_tag':forms.TextInput(attrs={'class':'form-control'}),
 
             'author':forms.TextInput(attrs={'class':'form-control','value':'','id':'elder','type':'hidden'}),
 
-            #'author':forms.Select(attrs={'class':'form-control'}),
+            'author':forms.Select(attrs={'class':'form-control'}),
             'category':forms.Select(choices=choices_list,attrs={'class':'form-control'}),
             'body':forms.Textarea(attrs={'class':'form-control'}),
         }
